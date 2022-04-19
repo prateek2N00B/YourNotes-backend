52587bae3925290d313b9ce94eee7cf464c38738
DevPatel29 <devmpatel29@gmail.com>
2022-04-19 18:41:43 +0530

    mongodb and backend setup

---
+++ server.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const userRouter = require("./routes/userRouter");
+const notesRouter = require("./routes/notesRouter");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/users-api", userRouter);
+app.use("/notes-api", notesRouter);
+
+const URI = process.env.MONGODB_URI;
+mongoose.connect(
+  URI,
+  {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("Connected to MongoDB");
+  }
+);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
