375e5f4663f27e3a11af91258be283e2538e1516
DevPatel29 <devmpatel29@gmail.com>
2022-04-19 18:43:16 +0530

    added user logic api support

---
+++ routes/notesRouter.js
@@ -0,0 +1,119 @@
+const router = require("express").Router();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  try {
+    const token = req.header("Authorization");
+    if (!token) return res.status(400).json({ msg: "Invalid Authentication" });
+
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+      if (err) return res.status(400).json({ msg: "Authorization not valid." });
+
+      req.user = user;
+      next();
+    });
+  } catch (err) {
+    return res.status(500).json({ msg: err.message });
+  }
+};
+
+const notesSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: false,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+    user_id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Notes = mongoose.model("Notes", notesSchema);
+
+const getNotes = async (req, res) => {
+  try {
+    const notes = await Notes.find({ user_id: req.user.id });
+    res.json(notes);
+  } catch (err) {
+    return res.status(500).json({ msg: err.message });
+  }
+};
+
+const createNotes = async (req, res) => {
+  try {
+    const { title, content, date } = req.body;
+    const newNote = new Notes({
+      title: title,
+      content: content,
+      date: date,
+      user_id: req.user.id,
+      name: req.user.name,
+    });
+
+    await newNote.save();
+    res.json({ msg: "Created a Note", id: newNote._id });
+  } catch (err) {
+    return res.status(500).json({ msg: err.message });
+  }
+};
+
+const deleteNotes = async (req, res) => {
+  try {
+    await Notes.findByIdAndDelete(req.params.id);
+    res.json({ msg: "Deleted the Note" });
+  } catch (err) {
+    return res.status(500).json({ msg: err.message });
+  }
+};
+
+const updateNote = async (req, res) => {
+  try {
+    const { title, content, date } = req.body;
+    await Notes.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        title: title,
+        content: content,
+        date: date,
+      }
+    );
+    res.json({ msg: "Notes Updated" });
+  } catch (err) {
+    return res.status(500).json({ msg: err.message });
+  }
+};
+
+const getNote = async (req, res) => {
+  try {
+    const note = await Notes.findById(req.params.id);
+    res.json(note);
+  } catch (err) {
+    return res.status(500).json({ msg: err.message });
+  }
+};
+
+router.route("/").get(auth, getNotes).post(auth, createNotes);
+
+router
+  .route("/:id")
+  .get(auth, getNote)
+  .put(auth, updateNote)
+  .delete(auth, deleteNotes);
+
+module.exports = router;
---
+++ routes/userRouter.js
@@ -0,0 +1,92 @@
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      reequired: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Users = mongoose.model("Users", userSchema);
+
+const registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const user = await Users.findOne({ email: email });
+    if (user) return res.status(400).json({ msg: "The email already exists." });
+
+    const passwordHash = await bcrypt.hash(password, 10);
+    const newUser = new Users({
+      username: username,
+      email: email,
+      password: passwordHash,
+    });
+
+    await newUser.save();
+    res.json({ msg: "Sign Up Successfull" });
+  } catch (err) {
+    return res.status(500).json({ msg: err.message });
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await Users.find({ email: email });
+
+    if (user.length == 0)
+      return res.status(400).json({ msg: "User does not exists." });
+
+    const isMatch = await bcrypt.compare(password, user[0].password);
+    if (!isMatch) return res.status(400).json({ msg: "Incorrect password." });
+
+    const payload = { id: user[0]._id, name: user[0].username };
+    const token = jwt.sign(payload, process.env.TOKEN_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.json({ token });
+  } catch (err) {
+    return res.status(500).json({ msg: "this is called" });
+  }
+};
+
+const verifiedToken = (req, res) => {
+  try {
+    const token = req.header("Authorization");
+    if (!token) return res.send(false);
+
+    jwt.verify(token, process.env.TOKEN_SECRET, async (err, verified) => {
+      if (err) return res.send(false);
+
+      const user = await Users.findById(verified.id);
+      if (!user) return res.send(false);
+
+      return res.send(true);
+    });
+  } catch (err) {
+    return res.status(500).json({ msg: err.message });
+  }
+};
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/verify", verifiedToken);
+
+module.exports = router;
